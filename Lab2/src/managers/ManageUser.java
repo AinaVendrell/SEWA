b42845163d969e297296d38a2fd59122119ac0c7
Aina Vendrell <aina@MacBook-de-Aina.local>
2020-05-10 13:41:21 +0200

    local image

--- Lab2/src/managers/ManageUser.java
+++ Lab2/src/managers/ManageUser.java
@@ -28,25 +28,25 @@ public class ManageUser {
 	}
 	
 	
-	// Add new user
-	public void addUser(String nom, String surname, String gender, String bdate, String usr, String mail, String pwd) {
-		String query = "INSERT INTO users (name,surname,gender,birthday,userName,email,pwd) VALUES (?,?,?,?,?,?,?)";
-		PreparedStatement statement = null;
-		try {
-			statement = db.prepareStatement(query);
-			statement.setString(1,nom); //cambio a nom xq name dara problema en sql
-			statement.setString(2,surname);
-			statement.setString(3,gender);
-			statement.setString(4,bdate);
-			statement.setString(5,usr);
-			statement.setString(6,mail);
-			statement.setString(7,pwd);
-			statement.executeUpdate();
-			statement.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+    // Add new user
+    public void addUser(String name, String surname, String gender, String birthday, String userName, String email, String pwd) {
+        String query = "INSERT INTO users (name,surname,gender,birthday,userName,email,pwd) VALUES (?,?,?,?,?,?,?)";
+        PreparedStatement statement = null;
+        try {
+            statement = db.prepareStatement(query);
+            statement.setString(1,name); //cambio a nom xq name dara problema en sql
+            statement.setString(2,surname);
+            statement.setString(3,gender);
+            statement.setString(4,birthday);
+            statement.setString(5,userName);
+            statement.setString(6,email);
+            statement.setString(7,pwd);
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 	
 	/*Check if all the fields are filled correctly */
 	public boolean isComplete(User user) {
